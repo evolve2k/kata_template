cbb23b0871427507c8eaf5a39ac83d190018f6ef
Richie Khoo <evolve2k@gmail.com>
2013-09-04 18:17:05 +0800

    initial commit

---
+++ Gemfile
@@ -0,0 +1,8 @@
+source 'https://rubygems.org'
+
+group :test, :development do
+	gem 'guard'
+	gem 'guard-rspec'
+	gem 'rspec'
+	gem 'rb-fsevent'
+end
---
+++ spec/practice_1_spec.rb
@@ -0,0 +1,10 @@
+# Replace this example spec with your own code
+# or create another file that 
+# ends in "_spec.rb" in the spec directory.
+# For example: roman_numerals_spec.rb
+
+describe "Checking that RSpec is working correctly" do
+  it "Valid test is passing" do
+  	true.should eq true
+  end
+end
